5b3280d9910cfe0be73eb07f36d88b3aad14bd7f
harrisgeo88 <charilaoss@gmail.com>
2018-08-19 21:37:23 +0100

    added semtantic-ui for better styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import * as _ from 'lodash'
 import Dropzone from 'react-dropzone'
+import {Menu, Grid, Segment, Icon, Container} from 'semantic-ui-react'
 
 class App extends Component {
 
 	state = {
 		data: null,
+		file: '',
 		clicked: {}
 	}
 
@@ -17,9 +19,7 @@ class App extends Component {
 	}
 
 	reset = () => {
-		this.setState({
-			data: null
-		})
+		this.setState({data: null})
 	}
 
 	onDrop = (files) => {
@@ -27,7 +27,8 @@ class App extends Component {
 		reader.onload = () => {
 			const fileAsBinaryString = reader.result
 			this.setState({
-				data: JSON.parse(fileAsBinaryString)
+				data: JSON.parse(fileAsBinaryString),
+				file: files[0].name
 			})
 		}
 		reader.onabort = () => console.log('file reading was aborted')
@@ -38,8 +39,11 @@ class App extends Component {
 	renderDropzone = () => {
 		if (!_.size(this.state.data)) {
 			return <Dropzone className='dropzone' onDrop={this.onDrop} accept='.json'>
-				<h2>Add JSON</h2>
-				<p>Click or drop file here to upload.</p>
+				<Segment padded='very' textAlign='center' secondary>
+					<h1>Drag and drop JSON File Here to Upload</h1>
+					<p>or click to open window.</p>
+					<Icon name='cloud upload' size='huge' />
+				</Segment>
 			</Dropzone>
 		}
 	}
@@ -47,26 +51,39 @@ class App extends Component {
 	renderHouses = () => {
 		let {data, clicked} = this.state
 		if (_.size(data)) {
-			return<div>
-				<button onClick={this.reset}>Reset</button>
-				<br/>
-				{_.map(data, (d, i) => {
-					let {title, description, href, img} = d
-					return <div key={i} className={'entry' + (clicked[i] === true ? ' clicked' : '')} onClick={() => this.click(href, i)}>
-						<img src={img} alt='entry'/>
-						<h2 className='title'>{i+1}. {title}<small className='subtitle'>{description}</small></h2>
-					</div>
-				})}
-			</div>
+			return <Grid columns={2} divided>
+				<Grid.Row stretched>
+					{_.map(data, (d, i) => {
+						let {title, description, href, img} = d
+						return <Grid.Column key={i} className={clicked[i] === true ? 'clicked' : ''} onClick={() => this.click(href, i)}>
+							<h2 className='title'>{i+1}.{title}<small className='subtitle'>{description}</small></h2>
+							<img src={img} alt='entry'/>
+							<br/><br/><br/><br/><br/>
+						</Grid.Column>
+					})}
+				</Grid.Row>
+			</Grid>
 		}
 	}
-	
-	
+
+	renderNavbar = () => {
+		let {data, file} = this.state
+
+		return <Menu className='fixed' id='navbar'>
+			<Menu.Item header>Data viewer</Menu.Item>
+			{file !== '' && <Menu.Item active={true}>{file}</Menu.Item>}
+			<Menu.Menu position='right'>
+				{data && <Menu.Item onClick={this.reset}>Reset</Menu.Item>}
+			</Menu.Menu>
+		</Menu>
+	}
+
 	render() {
-		return <div className="App">
+		return <Container>
+			{this.renderNavbar()}
 			{this.renderHouses()}
 			{this.renderDropzone()}
-		</div>
+		</Container>
 	}
 }
 
